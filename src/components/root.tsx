c34471ced44423c36cdf90b5a4f6551c6cba1124
AndriyShkrobut <andrii.shkrobut@gmail.com>
2021-11-09 03:10:47 +0200

    Setup linter, formatter, pre-commit hook

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const Root: React.FC = () => {
-    return (
-        <React.StrictMode>
-            <h1>Hello World!</h1>
-        </React.StrictMode>
-    )
-}
+const Root: React.FC = () => (
+  <React.StrictMode>
+    <h1>Hello World!</h1>
+  </React.StrictMode>
+);
 
 export default Root;
